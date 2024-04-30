d3e52ff33eaa6d028e380d064fa8c1c7a2d3cfaf
Cormac McCann <mccannsean421@gmail.com>
2024-04-30 09:49:51 +0100

    home page updates

--- src/app/page.js
+++ src/app/page.js
@@ -8,44 +8,56 @@ export default function Home() {
 
   return (
     <div className="content-container">
-    <main className="">
+    <main>
       <div className="hero grid justify-items-center">
 
-      <div className="platform p-12 mt-48">
-        <h1 className={`text-3xl lg:text-7xl font-bold`}>Cormac McCann</h1>
-        {/* <div className="glitch-animation">Cormac McCann</div> */}
-
-        <h2 className="py-12 text-xl text-gray-400">Web Developer - Gamer - Music Nerd - Occasional Hiker</h2>
+      <div className="platform p-12 mt-48 max-w-4xl w-full">
+        <h1 className={`text-xl lg:text-3xl font-bold text-gray-400`}>Hi, I&apos;m Cormac McCann</h1>
+        <h2 className="py-12 text-7xl text-white">I build websites</h2>
         <div>
-          <Link className="hover:border-gray-400 hover:text-gray-400 font-semibold py-2 px-4 border border-white rounded duration-200 mr-3 hero-btn text-white" href="/work">See My Work</Link>
-          <Link className="hover:border-gray-400 hover:text-gray-400 font-semibold py-2 px-4 border border-white rounded duration-200 hero-btn text-white" href="/contact">Say Hi</Link>
+          <Link className="hover:text-gray-200 font-semibold py-2 px-4 rounded duration-200 mr-3 hero-btn text-white bg-gray-600" href="/work">See My Work</Link>
+        </div>
+      </div>
+    
+
+      <div className="p-24">
+        <h2 className="text-3xl uppercase font-bold capitalize text-white pb-24 w-full text-center ">About me</h2>
+        <div className="p-12 content-block max-w-4xl">
+          <div className="pb-12">
+          <h3 className="text-white text-2xl pb-6">Who I am</h3>
+          <p className="text-gray-300 pb-12">
+            As mentioned above my name is Cormac and I am a web developer from Kildare, Ireland. 
+            I&apos;ve been building first class applications since 2016 where I got my start building 
+            static affiliate marketing websites. Over the years that grew into working on complex applications
+            dealing with everything from sports odds to medical data, from native apps to e-commerce backends.
+          </p>
+          <hr />
+          </div>
+          <div className="text-right">
+          <h3 className="text-white text-2xl pb-6">What I do</h3>
+            <p className="text-gray-300 pb-12">
+              As mentioned above my name is Cormac and I am a web developer from Kildare, Ireland. 
+              I&apos;ve been building first class applications since 2016 where I got my start building 
+              static affiliate marketing websites. Over the years that grew into working on complex applications
+              dealing with everything from sports odds to medical data, from native apps to e-commerce backends.
+            </p>
+            <hr />
+          </div>
+          <div className="pt-12">
+          <h3 className="text-white text-2xl pb-6">Why I do it</h3>
+            <p className="text-gray-300 pb-12">
+              As mentioned above my name is Cormac and I am a web developer from Kildare, Ireland. 
+              I&apos;ve been building first class applications since 2016 where I got my start building 
+              static affiliate marketing websites. Over the years that grew into working on complex applications
+              dealing with everything from sports odds to medical data, from native apps to e-commerce backends.
+            </p>
+            <hr />
+          </div>
         </div>
       </div>
 
       <div className="p-24">
-        <h2 className="text-3xl uppercase font-bold capitalize text-white pb-24 w-full text-center ">My Story</h2>
-        <div className="p-12 content-block max-w-4xl">
-          <div className="pb-12">
-          <h3 className="text-white text-2xl pb-6">Who am I?</h3>
-          <p className="text-gray-300 pb-12">
-            As mentioned above my name is Cormac and I am a web developer from Kildare, Ireland. 
-            I&apos;ve been building first class applications since 2016 where I got my start building 
-            static affiliate marketing websites. Over the years that grew into working on complex applications
-            dealing with everything from sports odds to medical data, from native apps to e-commerce backends.
-          </p>
-          <hr />
-          </div>
-          <div>
-          <h3 className="text-white text-2xl pb-6">Who am I?</h3>
-          <p className="text-gray-300 pb-12">
-            As mentioned above my name is Cormac and I am a web developer from Kildare, Ireland. 
-            I&apos;ve been building first class applications since 2016 where I got my start building 
-            static affiliate marketing websites. Over the years that grew into working on complex applications
-            dealing with everything from sports odds to medical data, from native apps to e-commerce backends.
-          </p>
-          <hr />
-          </div>
-        </div>
+        <h2 className="text-3xl uppercase font-bold capitalize text-white pb-24 w-full text-center ">Want to Connect?</h2>
       </div>
 
       </div>
